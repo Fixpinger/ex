21059cd53e4b11b8141d28b9e5c111924f826459
Fixpinger <117447416+Fixpinger@users.noreply.github.com>
2022-11-08 16:49:59 +0300

    Update Check2.java

--- src/Check2.java
+++ src/Check2.java
@@ -10,23 +10,55 @@ public class Check2 {
         boolean check = true;
         String[] arr1 = a.split("");
         Integer[] arrInt = new Integer[arr1.length];
-        String[] arr2 = a.split("");
+        String[] arr2 = b.split("");
         Integer[] arr2Int = new Integer[arr1.length];
 
         int i;
-        for(i = 0; i < arr1.length; ++i) {
-            arrInt[i] = (Integer)romanConvert2.mapNum.get(arr1[i]);
+        for (i = 0; i < arr1.length; ++i) {
+            arrInt[i] = (Integer) romanConvert2.mapNum.get(arr1[i]);
             if (arrInt[i] == null) {
                 check = false;
             }
         }
 
-        for(i = 0; i < arr2.length; ++i) {
-            arr2Int[i] = (Integer)romanConvert2.mapNum.get(arr1[i]);
+        for (i = 0; i < arr2.length; ++i) {
+            arr2Int[i] = (Integer) romanConvert2.mapNum.get(arr1[i]);
             if (arr2Int[i] == null) {
                 check = false;
             }
         }
+        int count = 0;
+        for (int j = 0; j < arr1.length; j++) {
+            String c = arr1[j];
+            count = 0;
+            for (int k = 1; k < arr1.length; k++) {
+                if (c.equals(arr1[k])){
+                    count++;
+                    if (count > 3){
+                        throw new ArithmeticException("Такого числа нет");
+                    }
+                }
+                else {
+                    count = 0;
+                }
+            }
+        }
+        for (int j = 0; j < arr2.length; j++) {
+            String c = arr2[j];
+            count = 0;
+            for (int k = 1; k < arr2.length - 1; k++) {
+                if (c.equals(arr2[k])){
+                    count++;
+                    if (count > 3){
+                        throw new ArithmeticException("Такого числа нет");
+                    }
+                }
+                else {
+                    count = 0;
+                }
+            }
+        }
+
 
         if (!arr1[0].equals("-") && !arr2[0].equals("-")) {
             return check;
@@ -34,6 +66,8 @@ public class Check2 {
             throw new Exception("т.к. в римской системе нет отрицательных чисел");
         }
     }
+
+
 
     public static boolean Check2(String a, String b) {
         try {
